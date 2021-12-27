639e52bbc3aef08e89519d4f117bef6addd1df07
Martin Seon <hyunmui@outlook.kr>
2021-12-27 22:40:07 +0900

    example 7-9

--- src/lib.rs
+++ src/lib.rs
@@ -14,13 +14,13 @@ mod front_of_house {
     }
 }
 
-pub fn eat_at_restaurant() {
-    // absolute path
-    crate::front_of_house::hosting::add_to_waitlist();
+// pub fn eat_at_restaurant() {
+//     // absolute path
+//     crate::front_of_house::hosting::add_to_waitlist();
 
-    // relative path
-    front_of_house::hosting::add_to_waitlist();
-}
+//     // relative path
+//     front_of_house::hosting::add_to_waitlist();
+// }
 
 fn serve_order() {}
 
@@ -31,6 +31,33 @@ mod back_of_house {
     }
 
     fn cook_order() {}
+
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("복숭아"),
+            }
+        }
+    }
+}
+
+pub fn eat_at_restaurant() {
+    // 여름에 아침 식사로 호밀빵을 주문한다.
+    let mut meal = back_of_house::Breakfast::summer("호밀빵");
+
+    // 고객이 마음을 바꿔서 빵 종류를 바꾼다.
+    meal.toast = String::from("밀빵");
+    println!("{} 토스트로 주세요", meal.toast);
+
+    // 다음 줄의 주석을 해제하면 컴파일 되지 않는다.
+    // 고객은 식사와 함께 제공되는 계절 과일을 선택할 수 없다.
+    // meal.seasonal_fruit = String::from("블루베리");
 }
 
 #[cfg(test)]
